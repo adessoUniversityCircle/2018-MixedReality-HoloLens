5548aa6eee7f3fd86a84ba8a74dccd6c0f8645ab
thomasrichter <mail@thomas-richter.info>
2018-06-16 21:04:54 +0200

    adesso Beispielprojekt hinzugefügt

---
+++ beispielprojekt/Assets/WorkshopProjects/WS_Rostock_2017_06_07-09/Spinning_Adesso_Logo/Scripts/LogoAnimator.cs
@@ -0,0 +1,41 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LogoAnimator : MonoBehaviour, IInputClickHandler {
+    private bool isOut;
+    private bool isMoving;
+    private Vector3 pos1;
+    public float distance = 1;
+    private Vector3 direction;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if (isOut == false)
+        {
+            pos1 = transform.position;
+            direction = new Vector3(pos1.x, 0, pos1.z);
+            isMoving = true;
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+        if (isMoving)
+        {
+            transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime / 10);
+
+            if ((transform.position - pos1).magnitude >= distance)
+            {
+                isMoving = false;
+                isOut = true;
+            }
+
+        }
+        else if (isOut)
+        {
+            this.GetComponentsInParent<Transform>(false)[1].Rotate(new Vector3(0, 1, 0), Time.deltaTime*25);
+        }
+	}
+}
---
+++ beispielprojekt/Assets/WorkshopProjects/WS_Rostock_2017_06_07-09/Target_Sphere/Scripts/AddKlotzOnClickChanged.cs
@@ -0,0 +1,22 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AddKlotzOnClickChanged : MonoBehaviour, IInputClickHandler
+{
+
+    public GameObject klotzPrefab1;
+    public GameObject klotzPrefab2;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        
+        KlotzManager.Instance.AddKlotz(klotzPrefab1);
+        KlotzManager.Instance.AddKlotz(klotzPrefab2);
+
+    }
+
+}
+
